494d4d0f039ce42719ac0b6798b1911242ffa745
Kryszczyn99 <krzysiopijanowski@gmail.com>
2021-10-23 06:09:53 +0200

    Home layout finished

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -9,7 +9,6 @@ import 'package:clinico/services/auth.dart';
 import 'package:clinico/style/colors.dart';
 
 class Home extends StatelessWidget {
-
   final AuthService _auth = AuthService();
 
   @override
@@ -18,21 +17,26 @@ class Home extends StatelessWidget {
       resizeToAvoidBottomInset: true,
       backgroundColor: MyColors.steelTeal,
       appBar: AppBar(
-      title: Text('Clinico'),
-      backgroundColor: MyColors.darkSkyBlue, 
-      elevation: 0.0,
+        title: Text('Clinico'),
+        backgroundColor: MyColors.darkSkyBlue,
+        elevation: 0.0,
       ),
       drawer: new Drawer(
         child: ListView(
           children: [
             new ListTile(
               tileColor: MyColors.darkSkyBlue,
-              leading: Icon(Icons.info,color: Colors.white,),
+              leading: Icon(
+                Icons.info,
+                color: Colors.white,
+              ),
               title: new Text('About Page'),
-              onTap: (){Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => About()),
-                  );},
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => About()),
+                );
+              },
               trailing: Wrap(
                 children: <Widget>[
                   Icon(Icons.arrow_forward), // icon-1
@@ -41,9 +45,14 @@ class Home extends StatelessWidget {
             ),
             new ListTile(
               tileColor: MyColors.darkSkyBlue,
-              leading: Icon(Icons.person,color: Colors.white,),
+              leading: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
               title: new Text('Sign out'),
-              onTap: () async {await _auth.signOut();},
+              onTap: () async {
+                await _auth.signOut();
+              },
               trailing: Wrap(
                 children: <Widget>[
                   Icon(Icons.arrow_forward), // icon-1
@@ -55,103 +64,138 @@ class Home extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 50,),
-            Container(
-                height: 150.0,
-                width: 150.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                        'assets/images/logo.jpg'),
-                    fit: BoxFit.fill,
-                  ),
-                  shape: BoxShape.circle,
-                ),
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 10,
               ),
-            SizedBox(height: 50,),
-                 Text(
+              Text(
                 'Dashboard',
                 style: TextStyle(
                   fontSize: 35,
                   color: Colors.white,
                 ),
-                ),
-            SizedBox(height:50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: (){Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => DoctorsView()),
-                  );},
-                  child:Container(
-                    height: 110,
-                    width: 110,
-                    decoration: new BoxDecoration(
-                      color: MyColors.warmBlack,
-                    ),
-                    child: Icon(Icons.card_travel,color: Colors.white,),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                height: 150.0,
+                width: 150.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/logo.jpg'),
+                    fit: BoxFit.fill,
                   ),
+                  shape: BoxShape.circle,
                 ),
-                SizedBox(width: 30,),
-                GestureDetector(
-                  onTap: (){Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ReceiptView()),
-                  );},
-                  child:Container(
-                    height: 110,
-                    width: 110,
-                    decoration: new BoxDecoration(
-                      color: MyColors.prussianBlue,
+              ),
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => DoctorsView()),
+                      );
+                    },
+                    child: Container(
+                      height: 130,
+                      width: 130,
+                      decoration: new BoxDecoration(
+                        color: MyColors.warmBlack,
+                        shape: BoxShape.circle,
+                        border: Border.all(width: 3, color: Colors.white),
+                      ),
+                      child: Icon(
+                        Icons.person,
+                        color: Colors.grey,
+                        size: 100,
+                      ),
                     ),
-                    child: Icon(Icons.local_hospital,color: Colors.white,),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: (){Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Calendar()),
-                  );},
-                  child:Container(
-                    height: 110,
-                    width: 110,
-                    decoration: new BoxDecoration(
-                      color: MyColors.mountainMeadow,
+                  SizedBox(
+                    width: 30,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ReceiptView()),
+                      );
+                    },
+                    child: Container(
+                      height: 130,
+                      width: 130,
+                      decoration: new BoxDecoration(
+                          color: MyColors.prussianBlue,
+                          shape: BoxShape.circle,
+                          border: Border.all(width: 3, color: Colors.white)),
+                      child: Icon(
+                        Icons.settings,
+                        color: Colors.grey,
+                        size: 100,
+                      ),
                     ),
-                    child: Icon(Icons.receipt,color: Colors.white,),
                   ),
-                ),
-                SizedBox(width: 30,),
-                GestureDetector(
-                  onTap: (){Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Info()),
-                  );},
-                  child: Container(
-                    height: 110,
-                    width: 110,
-                    decoration: new BoxDecoration(
-                      color: MyColors.darkSkyBlue,
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Calendar()),
+                      );
+                    },
+                    child: Container(
+                      height: 130,
+                      width: 130,
+                      decoration: new BoxDecoration(
+                          color: MyColors.mountainMeadow,
+                          border: Border.all(width: 3, color: Colors.white),
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/plus_icon.png'),
+                            fit: BoxFit.fill,
+                          ),
+                          shape: BoxShape.circle),
                     ),
-                    child: Icon(Icons.calendar_today,color: Colors.white,),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(
+                    width: 30,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Info()),
+                      );
+                    },
+                    child: Container(
+                      height: 130,
+                      width: 130,
+                      decoration: new BoxDecoration(
+                          color: MyColors.darkSkyBlue,
+                          border: Border.all(width: 3, color: Colors.white),
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/location.png'),
+                          ),
+                          shape: BoxShape.circle),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-      ),
+        ),
       ),
     );
   }

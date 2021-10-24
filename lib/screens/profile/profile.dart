c8e013d28ea2ba2a4e9b7174b31613e75b515994
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-10-24 20:40:31 +0200

    Base profile scaffold

---
+++ lib/screens/profile/profile.dart
@@ -0,0 +1,25 @@
+import 'package:clinico/services/database.dart';
+import 'package:clinico/style/colors.dart';
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MyColors.steelTeal,
+      appBar: AppBar(
+        backgroundColor: MyColors.darkSkyBlue,
+        elevation: 0.0,
+        title: Text("Profile"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}

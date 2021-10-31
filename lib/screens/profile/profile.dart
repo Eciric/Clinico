8c7cf5a0da50642fa3fd27342209b77cdef41609
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-10-31 11:44:49 +0100

    Changed the way user card is created

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -26,82 +26,67 @@ class ProfileView extends StatelessWidget {
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (snapshot.hasData) {
               var user = snapshot.data.docs[0];
-              return Stack(children: [
-                Positioned(
-                  top: 80,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 50),
-                      margin: EdgeInsets.symmetric(horizontal: 50),
-                      width: MediaQuery.of(context).size.width * 4 / 5,
-                      height: MediaQuery.of(context).size.width * 1 / 5 +
-                          avatarHeight,
-                      decoration: BoxDecoration(
-                          color: MyColors.mountainMeadow,
-                          borderRadius: BorderRadius.circular(25)),
+              return Stack(
+                children: <Widget>[
+                  Align(
+                      alignment: Alignment.topCenter,
                       child: Column(
                         children: [
-                          SizedBox(height: avatarHeight / 1.45),
-                          Text('Doctor / Patient',
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.white)),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text('${user.get('name')} ${user.get('surname')}',
-                              style:
-                                  TextStyle(fontSize: 35, color: Colors.white)),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text('${user.get('email')}',
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.white))
+                          Container(
+                              margin: EdgeInsets.symmetric(
+                                  vertical: MediaQuery.of(context).size.width *
+                                      1 /
+                                      10),
+                              decoration: BoxDecoration(
+                                  color: MyColors.mountainMeadow,
+                                  borderRadius: BorderRadius.circular(25)),
+                              width: MediaQuery.of(context).size.width * 4 / 5,
+                              height: avatarHeight + 115,
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Container(
+                                    height: avatarHeight,
+                                    width: avatarWidth,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/images/doctor.jpg'),
+                                        fit: BoxFit.fill,
+                                      ),
+                                      shape: BoxShape.circle,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text('Doctor / Patient',
+                                      style: TextStyle(
+                                          fontSize: 17, color: Colors.white)),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(
+                                      '${user.get('name')} ${user.get('surname')}',
+                                      style: TextStyle(
+                                          fontSize: 21, color: Colors.white)),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text('${user.get('email')}',
+                                      style: TextStyle(
+                                          fontSize: 17, color: Colors.white))
+                                ],
+                              )),
+                          Container(
+                            child: Text("TEST"),
+                          )
                         ],
                       )),
-                ),
-                Positioned(
-                  top: 25,
-                  left: MediaQuery.of(context).size.width / 2 - avatarWidth / 2,
-                  child: Container(
-                    height: avatarHeight,
-                    width: avatarWidth,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/doctor.jpg'),
-                        fit: BoxFit.fill,
-                      ),
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                ),
-                Positioned(
-                    top: 325,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            Column(
-                              children: [
-                                Text('My id'),
-                                Text(
-                                    '${user.get('userId').toString().substring(0, 16)}')
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Text('Phone number'),
-                                Text('${user.get('phoneNumber')}')
-                              ],
-                            )
-                          ],
-                        )
-                      ],
-                    ))
-              ]);
+                ],
+              );
             } else {
               return Container();
             }
@@ -110,3 +95,80 @@ class ProfileView extends StatelessWidget {
     );
   }
 }
+
+// Stack(children: [
+//                 Positioned(
+//                   top: 80,
+//                   left: 0,
+//                   right: 0,
+//                   child: Container(
+//                       padding: EdgeInsets.symmetric(horizontal: 50),
+//                       margin: EdgeInsets.symmetric(horizontal: 50),
+//                       width: MediaQuery.of(context).size.width * 4 / 5,
+//                       height: MediaQuery.of(context).size.width * 1 / 5 +
+//                           avatarHeight,
+//                       decoration: BoxDecoration(
+//                           color: MyColors.mountainMeadow,
+//                           borderRadius: BorderRadius.circular(25)),
+//                       child: Column(
+//                         children: [
+//                           SizedBox(height: avatarHeight / 1.45),
+//                           Text('Doctor / Patient',
+//                               style:
+//                                   TextStyle(fontSize: 17, color: Colors.white)),
+//                           SizedBox(
+//                             height: 10,
+//                           ),
+//                           Text('${user.get('name')} ${user.get('surname')}',
+//                               style:
+//                                   TextStyle(fontSize: 21, color: Colors.white)),
+//                           SizedBox(
+//                             height: 10,
+//                           ),
+//                           Text('${user.get('email')}',
+//                               style:
+//                                   TextStyle(fontSize: 17, color: Colors.white))
+//                         ],
+//                       )),
+//                 ),
+//                 Positioned(
+//                   top: 25,
+//                   left: MediaQuery.of(context).size.width / 2 - avatarWidth / 2,
+//                   child: Container(
+//                     height: avatarHeight,
+//                     width: avatarWidth,
+//                     decoration: BoxDecoration(
+//                       image: DecorationImage(
+//                         image: AssetImage('assets/images/doctor.jpg'),
+//                         fit: BoxFit.fill,
+//                       ),
+//                       shape: BoxShape.circle,
+//                     ),
+//                   ),
+//                 ),
+//                 Positioned(
+//                     top: 325,
+//                     child: Column(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       crossAxisAlignment: CrossAxisAlignment.center,
+//                       children: [
+//                         Row(
+//                           children: [
+//                             Column(
+//                               children: [
+//                                 Text('My id'),
+//                                 Text(
+//                                     '${user.get('userId').toString().substring(0, 16)}')
+//                               ],
+//                             ),
+//                             Column(
+//                               children: [
+//                                 Text('Phone number'),
+//                                 Text('${user.get('phoneNumber')}')
+//                               ],
+//                             )
+//                           ],
+//                         )
+//                       ],
+//                     ))
+//               ]);

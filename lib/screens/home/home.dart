4728e4ec9a650df83ac033d35fac0d8b75ed665f
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-11-24 08:10:07 +0100

    Added doctor creating appointment for role doctor

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,8 +1,12 @@
 import 'package:clinico/screens/about/about.dart';
+import 'package:clinico/screens/doctor/appointmentCreating.dart';
 import 'package:clinico/screens/home/homePage.dart';
+import 'package:clinico/screens/home/signOutMock.dart';
 import 'package:clinico/services/auth.dart';
 import 'package:clinico/screens/settings/settings.dart';
+import 'package:clinico/services/database.dart';
 import 'package:clinico/style/colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -13,37 +17,57 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _currentIndex = 0;
+  int _currentIndex = 1;
 
   List _screens = [
+    SignOutMock(),
     HomePage(),
-    About(),
     SettingsView(),
   ];
 
   List<BottomNavigationBarItem> _navItems = [
     BottomNavigationBarItem(
-      label: "Home",
-      icon: Icon(Icons.home),
+      label: "Sign out",
+      icon: Icon(Icons.logout),
     ),
     BottomNavigationBarItem(
-      label: "About",
-      icon: Icon(Icons.info_outline),
+      label: "Home",
+      icon: Icon(Icons.home),
     ),
     BottomNavigationBarItem(
       label: "Settings",
       icon: Icon(Icons.settings),
     ),
-    BottomNavigationBarItem(
-      label: "Sign out",
-      icon: Icon(Icons.logout),
-    ),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    DatabaseService()
+        .userCollection
+        .where('userId', isEqualTo: FirebaseAuth.instance.currentUser.uid)
+        .snapshots()
+        .first
+        .then((user) => {
+              if (user.docs.first.get("role") == "doctor")
+                {
+                  setState(() {
+                    _navItems.add(
+                      BottomNavigationBarItem(
+                        label: "Doctor panel",
+                        icon: Icon(Icons.list_alt),
+                      ),
+                    );
+                    _screens.add(DoctorAppointmentCreating());
+                  }),
+                }
+            });
+  }
 
   void _updateIndex(int value) {
     setState(() {
       _currentIndex = value;
-      if (_currentIndex == 3) {
+      if (_currentIndex == 0) {
         AuthService().signOut();
       }
     });
@@ -63,7 +87,7 @@ class _HomeState extends State<Home> {
         selectedFontSize: 13,
         unselectedFontSize: 13,
         iconSize: 30,
-        items: _navItems,
+        items: List.of(_navItems),
       ),
     );
   }
